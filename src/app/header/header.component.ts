2a8c8cb49403d20c4892a3641abfb0c62549bd66
omarmansor <omansor360@gmail.com>
2025-05-09 16:14:54 +0300

    add footer

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  openMenu: boolean = false;
 
+  toggle() {
+    this.openMenu = !this.openMenu;
+    console.log(this.openMenu);
+  }
 }
